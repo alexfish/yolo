49fb1ce88f8dbd51f31cf6a511e4ed44481cbc48
Alex Fish <alex@alexefish.com>
2013-01-20 00:41:21 +0000

    Added simple rake task to release

--- lib/yolo/tasks/ios/release.rb
+++ lib/yolo/tasks/ios/release.rb
@@ -6,6 +6,23 @@ module Yolo
     module Ios
       class Release < XcodeBuild::Tasks::BuildTask
 
+        attr_accessor :provisioning_profile
+
+        def initialize
+          self.sdk = "iphonesimulator" unless sdk
+          super
+        end
+
+        def define
+          namespace :yolo do
+            namespace :ios do
+              desc "Builds a release build of specified target(s)."
+              task :release do
+                xcodebuild :build
+              end
+            end
+          end
+        end
       end
     end
   end
