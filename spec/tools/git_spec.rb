37bafb173196f120a86af45add616b53e5376e32
Alex Fish <alex@alexefish.com>
2013-04-15 23:03:26 +0100

    Adding calbash spec and git pending tests

--- spec/tools/git_spec.rb
+++ spec/tools/git_spec.rb
@@ -1,8 +1,34 @@
-# bowling_spec.rb
 require 'yolo/tools/git'
 
-describe Yolo::Tools::Git, "Git" do
-  it "should pass" do
-    0.should eq(0)
+describe Yolo::Tools::Git do
+
+  pending "should check for new commits" do
+  end
+
+  pending "should check for new tags" do
+  end
+
+  pending "should keep track of the latest tag" do
+  end
+
+  pending "should keep track of the latest commit" do
+  end
+
+  pending "should store the latest tag" do
+  end
+
+  pending "should store the latest commit" do
+  end
+
+  pending "should save data to disk" do
+  end
+
+  pending "should get the latest tag" do
+  end
+
+  pending "should get the latest commit" do
+  end
+
+  pending "should get the current branch" do
   end
 end
---
+++ spec/tools/ios/calabash_spec.rb
@@ -0,0 +1,8 @@
+require 'yolo/tools/ios/calabash'
+
+describe Yolo::Tools::Ios::Calabash do
+
+  pending "should execute the cucumber command" do
+  end
+
+end
