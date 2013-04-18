3b88e65d5dbe8b319c2786b00565a61249a98712
Alex Fish <alex@alexefish.com>
2013-04-18 21:52:53 +0100

    Adding Tools::Ios::Calabash tests

--- spec/tools/ios/calabash_spec.rb
+++ spec/tools/ios/calabash_spec.rb
@@ -1,8 +1,29 @@
+require 'spec_helper'
 require 'yolo/tools/ios/calabash'
 
 describe Yolo::Tools::Ios::Calabash do
 
-  pending "should execute the cucumber command" do
+  before do
+    IO.stub(:popen)
+  end
+
+  describe "when running " do
+
+    it "should execute the cucumber command" do
+      IO.should_receive(:popen).with(/cucumber/)
+      Yolo::Tools::Ios::Calabash.run()
+    end
+
+    it "should pass junit as the default format" do
+      IO.should_receive(:popen).with(/--format junit/)
+      Yolo::Tools::Ios::Calabash.run()
+    end
+
+    it "pass test-reports/cucumber as the default output directory" do
+      IO.should_receive(:popen).with(/--out test-reports\/cucumber/)
+      Yolo::Tools::Ios::Calabash.run()
+    end
+
   end
 
 end
