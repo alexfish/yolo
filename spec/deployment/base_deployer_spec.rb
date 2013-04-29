150bc7fb60fca2211a064fd88164ca9fa5385cd3
Alex Fish <alex@alexefish.com>
2013-04-29 20:47:09 +0100

    Adding base deployer spec tests

--- spec/deployment/base_deployer_spec.rb
+++ spec/deployment/base_deployer_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 require 'yolo/deployment'
 require 'yolo/formatters'
+require 'yolo/config'
 
 describe Yolo::Deployment::BaseDeployer do
-  pending "all tests" do
+
+  describe "when initilized" do
+    it "should load a deploy url from settings" do
+      Yolo::Config::Settings.instance.stub(:deploy_url){"test_url"}
+      @deployer = Yolo::Deployment::BaseDeployer.new
+      @deployer.url.should eq("test_url")
+    end
   end
+
 end
