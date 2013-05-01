a2b11d14a78f2116dddcf782503124eba506e687
Alex Fish <fish@ustwo.co.uk>
2013-05-01 11:16:22 +0100

    Adding logic to skip codesigning for debug builds

--- lib/yolo/tasks/ios/build.rb
+++ lib/yolo/tasks/ios/build.rb
@@ -7,6 +7,23 @@ module Yolo
       # @author [Alex Fish]
       #
       class Build < Yolo::Tasks::BaseTask
+
+        #
+        # Overrides the superclass build_opts_string method and appends skip code sign command
+        # if debug confid is defined
+        #
+        # @param  additional_opts [Array] an array of additional options for the build command
+        #
+        # @return [String] the option string with additional options and test output
+        # pipe appended if defined
+        def build_opts_string(*additional_opts)
+          options = build_opts + additional_opts
+          if configuration == "Debug" or configuration.length == 0
+            options = options << "CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO"
+          end
+          return options.compact.join(" ")
+        end
+
         #
         # Defines rake tasks available to the Build class
         #
