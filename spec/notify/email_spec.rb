05f6b33b75d6be16d8d14f21091ed8bbb4cc3317
Alex Fish <alex@alexefish.com>
2013-05-07 19:55:08 +0100

    Completing email tests

--- spec/notify/email_spec.rb
+++ spec/notify/email_spec.rb
@@ -16,6 +16,8 @@ describe Yolo::Notify::Email do
     @settings.any_instance.stub(:mail_account){"test_account"}
     @settings.any_instance.stub(:mail_password){"test_password"}
     @email = Yolo::Notify::Email.new
+    @email.stub(:body){"test body"}
+    @email.to = "test_to"
   end
 
   describe "when initilized" do
@@ -40,8 +42,87 @@ describe Yolo::Notify::Email do
     end
   end
 
-  pending "send tests" do
-    it "needs to be written" do
+  describe "when sending" do
+    before do
+      @smtp = mock(Net::SMTP)
+      Net::SMTP.stub(:new){@smtp}
+      @smtp.stub(:start).and_yield
+      @smtp.stub(:send_message)
+      @smtp.stub(:enable_starttls)
+      @options = {}
+    end
+
+    describe "with an account" do
+      it "should send with tls" do
+        @smtp.should_receive(:enable_starttls)
+        @email.send(@options)
+      end
+
+      it "should send" do
+        @smtp.should_receive(:send_message).with("test body", "test_from", "test_to")
+        @email.send(@options)
+      end
+    end
+
+    describe "without an account" do
+      before do
+        @smtp.stub(:start).and_yield(@smtp)
+        @email.account = nil
+      end
+
+      it "should not send with tls" do
+        @smtp.should_not_receive(:enable_starttls)
+        @email.send(@options)
+      end
+
+      it "should send" do
+        @smtp.should_receive(:send_message).with("test body", "test_from", "test_to")
+        @email.send(@options)
+      end
+    end
+
+    describe "and loading options" do
+
+      before do
+        @email.to = "test_to"
+        @email.send(@options)
+      end
+
+      it "should load a server if missing" do
+        @options[:server].should eq("test_host")
+      end
+
+      it "should load a from address if missing" do
+        @options[:from].should eq("test_from")
+      end
+
+      it "should load a subject if missing" do
+        @options[:subject].should eq("New Build!")
+      end
+
+      it "should load a title if missing" do
+        @options[:title].should eq("New Build!")
+      end
+
+      it "should load a body if missing" do
+        @options[:body].should eq("test body")
+      end
+
+      it "should load a password if missing" do
+        @options[:password].should eq("test_password")
+      end
+
+      it "should load an account if missing" do
+        @options[:account].should eq("test_account")
+      end
+
+      it "should load a port if missing" do
+        @options[:port].should eq(666)
+      end
+
+      it "should load a to address if missing" do
+        @options[:to].should eq("test_to")
+      end
     end
   end
 
