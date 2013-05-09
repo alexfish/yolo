4471a7117282271ce48bef32cdf7923f2d52e29a
Alex Fish <alex@alexefish.com>
2013-05-09 19:24:31 +0100

    Adding OCUnit task tests

--- spec/tasks/ios/ocunit_spec.rb
+++ spec/tasks/ios/ocunit_spec.rb
@@ -1,5 +1,28 @@
 require 'yolo/tasks'
 
 describe Yolo::Tasks::Ios::OCUnit do
+  before do
+    @ocunit = Yolo::Tasks::Ios::OCUnit.new
+  end
 
+  describe "when created" do
+    it "should set the iphonesimulator as the default sdk" do
+      @ocunit.sdk.should eq("iphonesimulator")
+    end
+
+    it "should set junit as the default test output" do
+      @ocunit.test_output.should eq(:junit)
+    end
+  end
+
+  describe "when building an options string" do
+    it "should append additional options" do
+      @ocunit.build_opts_string("one","two","three").should match(/one two three/)
+    end
+
+    it "should pipe output to ocunit2junit" do
+      @ocunit.test_output = :junit
+      @ocunit.build_opts_string.should match(/2>&1 | ocunit2junit/)
+    end
+  end
 end
