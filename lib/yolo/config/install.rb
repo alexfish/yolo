9718ee534ae51948d030c9c86fcc0e46a754e997
Alex Fish <alex@alexefish.com>
2013-05-12 17:44:04 +0100

    Updating install docs

--- lib/yolo/config/install.rb
+++ lib/yolo/config/install.rb
@@ -10,6 +10,9 @@ module Yolo
     #
     class Install
 
+      #
+      # Runs the yolo project install
+      #
       def self.run
          self.move_rakefile
          self.rename_rakefile
@@ -19,14 +22,24 @@ module Yolo
 
       private
 
+      #
+      # Moves the default rakefile into the current directory
+      #
       def self.move_rakefile
-         FileUtils.cp_r(File.dirname(__FILE__) + "/yolo-Rakefile", Dir.pwd)
+         FileUtils.cp_r(self.rakefile, Dir.pwd)
       end
 
+      #
+      # The path to the default rake file
+      #
+      # @return [String] The full path to the default rakefile
       def self.rakefile
         File.dirname(__FILE__) + "/yolo-Rakefile"
       end
 
+      #
+      # Renames the default rakefile removing the yolo- prefix
+      #
       def self.rename_rakefile
         FileUtils.mv(Dir.pwd + '/yolo-Rakefile',Dir.pwd + '/Rakefile')
       end
---
+++ xcode-sample/Rakefile
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'yolo'
+
+#
+# Build Task
+#
+Yolo::Tasks::Ios::Build.new do |t|
+  t.workspace = "Yolo.xcworkspace"
+  t.scheme = "Yolo"
+end
+
+#
+# Release Task
+#
+# Yolo::Tasks::Ios::Release.new do |t|
+#   t.workspace = "MyProject.xcworkspace"
+#   t.scheme = "MyProject"
+#   t.configuration = "Release"
+# end
+
+#
+# OCUnit task
+#
+# Yolo::Tasks::Ios::OCUnit.new do |t|
+#   t.workspace = "MyProject.xcworkspace"
+#   t.scheme = "MyProjectTests"
+#   t.test_output = :junit
+# end
+
+#
+# Calabash task
+#
+# Yolo::Tasks::Ios::Calabash.new do |t|
+#   t.workspace = "MyProject.xcworkspace"
+#   t.scheme = "MyProject-cal"
+#   t.format = :junit
+#   t.output_dir = "example-tests"
+# end
+
+#
+# Coverage task
+#
+# Yolo::Tasks::Ios::Coverage.new do |t|
+#   t.scheme = "MyProject"
+# end
